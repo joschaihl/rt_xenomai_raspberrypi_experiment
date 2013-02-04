6aa55c4363fde48297a644be98e44cab66d564ea
joschaihl <joschaihl@git.code.sf.net>
2013-02-04 15:25:08 +0100

    Refactored ringbuffer_model

--- datarecorder_lib/xen_ringbuf_controller.h
+++ datarecorder_lib/xen_ringbuf_controller.h
@@ -3,4 +3,38 @@
 
 #include "xen_ringbuf_model.h"
 
+int init_shmem_consumer(void)
+{
+    int err;
+    /* Bind to a shared heap which has been created elsewhere, either
+       in kernel or user-space. Here we cannot wait and the heap must
+       be available at once, since the caller is not a Xenomai-enabled
+       thread. The heap should have been created with the H_SHARED
+       mode set. */
+    err = rt_heap_bind(&datarecorder_heap,SHM_NAME,TM_NONBLOCK);
+
+    if (err)
+    {
+        printf("Can't bind to shared memory");
+        return -1;
+    }
+    /* Get the address of the shared memory segment. The "size" and
+       "timeout" arguments are unused here. */
+
+    rt_heap_alloc(&datarecorder_heap,0,TM_NONBLOCK,&shmem);
+
+    ringBuffer = shmem;
+    return 0;
+}
+
+
+void cleanup (void)
+{
+    /* We need to unbind explicitly from the heap in order to
+       properly release the underlying memory mapping. Exiting the
+       process unbinds all mappings automatically. */
+    rt_heap_unbind(&datarecorder_heap);
+}
+
+
 #endif
--- xenomai_userspace_tools/read_shm.c
+++ xenomai_userspace_tools/read_shm.c
@@ -1,49 +1,19 @@
-#include <ringbuffer_model.h>
-#include <native/heap.h>
+#include <xen_ringbuf_controller.h>
 
-RT_HEAP heap_desc;
-
-void *shared_mem; /* Start address of the shared memory segment */
-
-/* A shared memory segment with Xenomai is implemented as a mappable
-   real-time heap object managed as a single memory block. In this
-   mode, the allocation routine always returns the start address of
-   the heap memory to all callers, and the free routine always leads
-   to a no-op. */
-
-void cleanup (void)
-{
-    /* We need to unbind explicitly from the heap in order to
-       properly release the underlying memory mapping. Exiting the
-       process unbinds all mappings automatically. */
-    rt_heap_unbind(&heap_desc);
-}
 
 int main (int argc, char *argv[])
 {
-    int err;
     unsigned long long i;
-    /* Bind to a shared heap which has been created elsewhere, either
-       in kernel or user-space. Here we cannot wait and the heap must
-       be available at once, since the caller is not a Xenomai-enabled
-       thread. The heap should have been created with the H_SHARED
-       mode set. */
-    err = rt_heap_bind(&heap_desc,SHM_NAME,TM_NONBLOCK);
-
-    if (err)
+    if(init_shmem_consumer()!=0)
     {
-        printf("Can't bind to shared memory");
-        return -1;
+    	printf("Error");
+    	return -1;
     }
-    /* Get the address of the shared memory segment. The "size" and
-       "timeout" arguments are unused here. */
-    rt_heap_alloc(&heap_desc,0,TM_NONBLOCK,&shared_mem);
-    
-    RingBuffer *rb = shared_mem;
-  	printf("Ringbuffer size = %llu, overflows = %llu\n", rb->size, rb->overflows);
-    for(i = 0; i < rb->size;i++)
+
+  	printf("Ringbuffer size = %llu, overflows = %llu\n", ringBuffer->size, ringBuffer->overflows);
+    for(i = 0; i < ringBuffer->size;i++)
     {
-        printf("Sample %llu: %u %llu %u\n", i, rb->sensorData[i].sensorID, rb->sensorData[i].sampleTimeCode, rb->sensorData[i].sensorValue);
+        printf("Sample %llu: %u %llu %u\n", i, ringBuffer->sensorData[i].sensorID, ringBuffer->sensorData[i].sampleTimeCode, ringBuffer->sensorData[i].sensorValue);
     }
     
     cleanup();
